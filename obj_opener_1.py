db65c12dd61e2c0cfa1f838e5c1b05bbc04ffc38
zaahraebadi <139007404+zaahraebadi@users.noreply.github.com>
2023-07-09 00:52:19 +0330

    Add files via upload
    گرفتن دایرکتوری از مخاطب

--- obj_opener_1.py
+++ obj_opener_1.py
@@ -1,14 +1,20 @@
 import trimesh
 import pyrender
 
-obj_file = r"C:\Users\zhr\Desktop\cup.obj"
+# Prompt user for the directory of the OBJ file
+obj_file = input("Enter the directory of the OBJ file: ")
+
+# Load OBJ file
 mesh = trimesh.load_mesh(obj_file)
 
+# Create a pyrender scene and add the mesh to it
 scene = pyrender.Scene()
 scene.add(pyrender.Mesh.from_trimesh(mesh))
 
+# Create a pyrender viewer
 viewer = pyrender.Viewer(scene, use_raymond_lighting=True)
 
+# Keep the viewer open until it's closed
 while not viewer.is_active:
     viewer.render_lock.release()
 
